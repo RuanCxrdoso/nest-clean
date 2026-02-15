33eb2e793745822dfb68f53b7eebc2aa3cef0eaf
Ruan Cardoso <cardosoruan2001@gmail.com>
2026-02-15 18:30:18 -0300

    :sparkles: feat: Add prisma service

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import {
   Injectable,
   Logger,
@@ -5,7 +6,7 @@ import {
   OnModuleInit,
 } from '@nestjs/common'
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from 'generated/prisma/client.js'
+import { PrismaClient } from 'generated/prisma/client'
 
 @Injectable()
 export class PrismaService
