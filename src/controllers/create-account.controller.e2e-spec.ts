a7b2a08e094a2bd608aa1a515f7f749a37e891e3
Ruan Cardoso <cardosoruan2001@gmail.com>
2026-02-18 23:28:11 -0300

    :test_tube: test: Add E2E tests for Auth and Create Account controllers

--- src/controllers/create-account.controller.e2e-spec.ts
+++ src/controllers/create-account.controller.e2e-spec.ts
@@ -1,5 +1,45 @@
-describe('Create account E2E tests', () => {
-  it('should be able to create an account', () => {
-    expect(1 + 1).toBe(2)
+import { PrismaService } from '@/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+
+describe('Create account [E2E]', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+
+  beforeAll(async () => {
+    const { AppModule } = await import('../app.module.js')
+    const { PrismaService } = await import('../prisma/prisma.service.js')
+
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    prisma = moduleRef.get(PrismaService)
+
+    await app.init()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  test('[POST] /accounts', async () => {
+    const response = await request(app.getHttpServer()).post('/accounts').send({
+      name: 'Ruan Cardoso E2E',
+      email: 'ruane2e@email.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toBe(201)
+
+    const userOnDataBase = await prisma.user.findFirst({
+      where: {
+        email: 'ruane2e@email.com',
+      },
+    })
+
+    expect(userOnDataBase).toBeTruthy()
   })
 })
