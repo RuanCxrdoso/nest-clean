b15164c5c66f32a5d515e7a471ecf2e14beddb21
Ruan Cardoso <cardosoruan2001@gmail.com>
2026-02-19 20:45:02 -0300

    :sparkles: feat: Implement DatabaseModule and PrismaService for database connection management

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,16 +2,16 @@ import { AuthController } from '@/infra/http/controllers/auth.controller'
 import { CreateAccountController } from '@/infra/http/controllers/create-account.controller'
 import { CreateQuestionController } from '@/infra/http/controllers/create-question.controller'
 import { FetchRecentQuestionsController } from '@/infra/http/controllers/fetch-recent-questions.controller'
-import { PrismaService } from '@/infra/prisma/prisma.service'
 import { Module } from '@nestjs/common'
+import { DatabaseModule } from '@/infra/database/database.module'
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [
     CreateAccountController,
     CreateQuestionController,
     FetchRecentQuestionsController,
     AuthController,
   ],
-  providers: [PrismaService],
 })
 export class HttpModule {}
