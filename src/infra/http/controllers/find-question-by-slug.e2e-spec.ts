586c44f4b7ada23d423376a2836f4c368f396979
Ruan Cardoso <cardosoruan2001@gmail.com>
2026-02-26 15:25:55 -0300

    :test_tube: test: Add E2E tests for Find Question by slug endpoint

---
+++ src/infra/http/controllers/find-question-by-slug.e2e-spec.ts
@@ -0,0 +1,65 @@
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+
+describe('Find Question by slug [E2E]', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+  let jwt: JwtService
+
+  beforeAll(async () => {
+    const { AppModule } = await import('@/infra/app.module.js')
+    const { PrismaService } =
+      await import('@/infra/database/prisma/prisma.service.js')
+    const { JwtService } = await import('@nestjs/jwt')
+
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    prisma = moduleRef.get(PrismaService)
+    jwt = moduleRef.get(JwtService)
+
+    await app.init()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  test('[GET] /questions/:slug', async () => {
+    const user = await prisma.user.create({
+      data: {
+        name: 'Ruan',
+        email: 'ruan@email.com',
+        password: '123456',
+      },
+    })
+
+    const accessToken = jwt.sign({ sub: user.id })
+
+    await prisma.question.create({
+      data: {
+        authorId: user.id,
+        slug: 'title-1',
+        title: 'Title-1',
+        content: 'Question content',
+      },
+    })
+
+    const response = await request(app.getHttpServer())
+      .get(`/questions/title-1`)
+      .set('Authorization', `Bearer ${accessToken}`)
+
+    expect(response.statusCode).toBe(200)
+    expect(response.body.question).toEqual(
+      expect.objectContaining({
+        slug: 'title-1',
+      }),
+    )
+  })
+})
