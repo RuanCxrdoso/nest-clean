55d51ed85e77ea23e49ba7842e2d535722636f00
Ruan Cardoso <cardosoruan2001@gmail.com>
2026-03-03 11:26:19 -0300

    :test_tube: test: Enhance update question test e2e with attachment handling

--- src/infra/http/controllers/update-question.controller.e2e-spec.ts
+++ src/infra/http/controllers/update-question.controller.e2e-spec.ts
@@ -1,10 +1,13 @@
+import { QuestionAttachmentList } from '@/domain/forum/enterprise/entities/question-attachment-list'
 import { DatabaseModule } from '@/infra/database/database.module'
 import { PrismaService } from '@/infra/database/prisma/prisma.service'
 import { INestApplication } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import { Test } from '@nestjs/testing'
 import request from 'supertest'
+import { AttachmentFactory } from 'test/factories/make-attachment'
 import { QuestionFactory } from 'test/factories/make-question'
+import { QuestionAttachmentFactory } from 'test/factories/make-question-attachment'
 import { StudentFactory } from 'test/factories/make-student'
 
 describe('Edit Question [E2E]', () => {
@@ -12,6 +15,8 @@ describe('Edit Question [E2E]', () => {
   let prisma: PrismaService
   let studentFactory: StudentFactory
   let questionFactory: QuestionFactory
+  let attachmentFactory: AttachmentFactory
+  let questionAttachmentFactory: QuestionAttachmentFactory
   let jwt: JwtService
 
   beforeAll(async () => {
@@ -22,7 +27,12 @@ describe('Edit Question [E2E]', () => {
 
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule, DatabaseModule],
-      providers: [StudentFactory, QuestionFactory],
+      providers: [
+        StudentFactory,
+        QuestionFactory,
+        AttachmentFactory,
+        QuestionAttachmentFactory,
+      ],
     }).compile()
 
     app = moduleRef.createNestApplication()
@@ -30,6 +40,8 @@ describe('Edit Question [E2E]', () => {
     jwt = moduleRef.get(JwtService)
     studentFactory = moduleRef.get(StudentFactory)
     questionFactory = moduleRef.get(QuestionFactory)
+    attachmentFactory = moduleRef.get(AttachmentFactory)
+    questionAttachmentFactory = moduleRef.get(QuestionAttachmentFactory)
 
     await app.init()
   })
@@ -46,12 +58,40 @@ describe('Edit Question [E2E]', () => {
 
     const accessToken = jwt.sign({ sub: user.id.toString() })
 
+    const attachment1 = await attachmentFactory.makePrismaAttachment()
+    const attachment2 = await attachmentFactory.makePrismaAttachment()
+
+    const questionAttachment1 =
+      await questionAttachmentFactory.makePrismaQuestionAttachment({
+        questionId: question.id,
+        attachmentId: attachment1.id,
+      })
+
+    const questionAttachment2 =
+      await questionAttachmentFactory.makePrismaQuestionAttachment({
+        questionId: question.id,
+        attachmentId: attachment2.id,
+      })
+
+    question.attachments = new QuestionAttachmentList([
+      questionAttachment1,
+      questionAttachment2,
+    ])
+
+    const attachment3 = await attachmentFactory.makePrismaAttachment()
+    const attachment4 = await attachmentFactory.makePrismaAttachment()
+
     const response = await request(app.getHttpServer())
       .put(`/questions/${question.id.toString()}`)
       .set('Authorization', `Bearer ${accessToken}`)
       .send({
         title: 'Updated title',
         content: 'Updated content',
+        attachmentsIds: [
+          attachment2.id.toString(),
+          attachment3.id.toString(),
+          attachment4.id.toString(),
+        ],
       })
 
     expect(response.statusCode).toBe(200)
@@ -64,5 +104,21 @@ describe('Edit Question [E2E]', () => {
     })
 
     expect(isQuestionOnDatabase).toBeTruthy()
+
+    const attachmentsOnDataBase = await prisma.attachment.findMany({
+      where: {
+        questionId: question.id.toString(),
+      },
+    })
+
+    expect(attachmentsOnDataBase).toHaveLength(3)
+
+    const attachmentdsIds = attachmentsOnDataBase.map((item) => item.id)
+
+    expect(attachmentdsIds).toEqual([
+      attachment2.id.toString(),
+      attachment3.id.toString(),
+      attachment4.id.toString(),
+    ])
   })
 })
